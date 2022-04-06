c292e6f2e663c9d1a82dcada481b56180fbb893b
Nitin Sharma <s1911nitin@gmail.com>
2022-04-06 11:43:34 +0530

    Updated

--- NewsApp/settings.py
+++ NewsApp/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,8 +140,8 @@ LOGIN_URL = "/login/"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 's19nitin@gmail.com'
+EMAIL_HOST_PASSWORD = 'czqfiqtdvdpqlzuj'
 EMAIL_USE_TLS = True
 
 # Added Caching feature
